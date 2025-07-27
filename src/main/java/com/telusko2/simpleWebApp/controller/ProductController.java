26c9c69e6284e073dd0535608fe1c9a5ed35d2e7
DESELMAAR <elmaarpro@gmail.com>
2025-07-27 23:51:04 +0100

    controller,service,model ...products endpoint work ..

--- src/main/java/com/telusko2/simpleWebApp/controller/ProductController.java
+++ src/main/java/com/telusko2/simpleWebApp/controller/ProductController.java
@@ -1,7 +1,18 @@
 package com.telusko2.simpleWebApp.controller;
 
+import com.telusko2.simpleWebApp.model.Product;
+import com.telusko2.simpleWebApp.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
 public class ProductController {
-    public String getProducts() {
-        return ("product page");
+    @Autowired
+    ProductService service;
+    @RequestMapping("/products")
+    public List<Product> getProducts() {
+        return service.getProducts();
     }
 }
--- src/main/java/com/telusko2/simpleWebApp/service/ProductService.java
+++ src/main/java/com/telusko2/simpleWebApp/service/ProductService.java
@@ -1,11 +1,21 @@
 package com.telusko2.simpleWebApp.service;
 
 import com.telusko2.simpleWebApp.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 
+@Service
 public class ProductService {
+
+    List<Product> products= Arrays.asList(
+            new Product(101,"Iphone",7000),
+            new Product(102, "Canon Camera", 4000),
+            new Product(103, "zaeis", 5000)
+    );
     public List<Product> getProducts() {
-          return null;
+          return products;
     }
 }
